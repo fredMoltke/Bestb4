1d2b24bee438d91a8b220ff6cd584e1a01c4e174
fmoltke <freggi64@live.dk>
2020-10-13 16:02:56 +0200

    påbegyndt RecyclerView implementering

---
+++ app/src/main/java/com/example/bestb4/ListAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.bestb4
+
+import androidx.recyclerview.widget.RecyclerView
+
+class ListAdapter : RecyclerView.Adapter() {
+
+    class ListViewHolder : RecyclerView.ViewHolder {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/bestb4/ListItem.kt
@@ -0,0 +1,3 @@
+package com.example.bestb4
+
+data class ListItem(val imageResource: Int, val text1: String, val text2: String)
