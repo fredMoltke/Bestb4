d841a944cce7da5d3966427b0099823bc424f0c8
fmoltke <freggi64@live.dk>
2020-10-13 16:25:07 +0200

    påbegyndt RecyclerView implementering

--- app/src/main/java/com/example/bestb4/ListActivity.kt
+++ app/src/main/java/com/example/bestb4/ListActivity.kt
@@ -2,10 +2,28 @@ package com.example.bestb4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_list.*
 
 class ListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+        val exampleList = generateDummyList(100)
+        recycler_view.adapter = ListAdapter(exampleList)
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        recycler_view.setHasFixedSize(true)
+    }
+
+    // Funktion der opretter liste med dummy items
+    private fun generateDummyList(size: Int): List<ListItem> {
+        val list = ArrayList<ListItem>()
+
+        for (i in 0 until size) {
+            val item = ListItem(R.drawable.ic_baseline_fastfood_24, "Item $i", "Line 2")
+            list += item
+        }
+        return list
     }
 }
--- app/src/main/java/com/example/bestb4/ListAdapter.kt
+++ app/src/main/java/com/example/bestb4/ListAdapter.kt
@@ -1,11 +1,39 @@
 package com.example.bestb4
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.list_item.view.*
 
-class ListAdapter : RecyclerView.Adapter() {
+// Guide brugt til implementering: https://www.youtube.com/watch?v=afl_i6uvvU0
+class ListAdapter(private val exampleList: List<ListItem>) : RecyclerView.Adapter<ListAdapter.ListViewHolder>() {
 
-    class ListViewHolder : RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_item,
+        parent, false)
 
+        return ListViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val currentItem = exampleList[position]
+
+        // Skal ændres for at indsætte billeder fra kamera?
+        holder.imageView.setImageResource(currentItem.imageResource)
+
+        holder.textView1.text = currentItem.text1
+        holder.textView2.text = currentItem.text2
+    }
+
+    override fun getItemCount() = exampleList.size
+
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.list_item_image_view
+        val textView1: TextView = itemView.list_item_text_view_1
+        val textView2: TextView = itemView.list_item_text_view_2
     }
 
 }
