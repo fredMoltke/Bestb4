a2f1a8c7823da4b52d23b18b97e1eb7e5c6e2a6e
fmoltke <freggi64@live.dk>
2020-12-03 05:53:31 +0100

    Farver vises på list items efter holdbarhed. App ikon ændret

--- app/src/main/java/com/app/bestb4/ListAdapter.kt
+++ app/src/main/java/com/app/bestb4/ListAdapter.kt
@@ -1,9 +1,11 @@
 package com.app.bestb4
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.app.bestb4.data.events.ClickEvent
@@ -27,23 +29,41 @@ class ListAdapter(private val exampleList: List<ListItem>) : RecyclerView.Adapte
         // Skal ændres for at indsætte billeder fra kamera?
         holder.imageView.setImageBitmap(currentItem.thumbnail)
 
+
         holder.textView1.text = currentItem.name
         when {
-            currentItem.daysLeft < -1 -> holder.textView2.text = "Udløbet! Holdbarhed overskredet med ${(currentItem.daysLeft)*(-1)} dage."
-            currentItem.daysLeft == -1 -> holder.textView2.text = "Udløbet! Holdbarhed overskredet med ${(currentItem.daysLeft)*(-1)} dag."
-            currentItem.daysLeft == 0 -> holder.textView2.text = "Udløber i dag."
-            currentItem.daysLeft == 1 -> holder.textView2.text = "Holdbar i ${currentItem.daysLeft} dag."
-            else -> holder.textView2.text = "Holdbar i ${currentItem.daysLeft} dage."
+                currentItem.daysLeft < -1 -> {
+                    holder.textView2.text = "Udløbet! Holdbarhed overskredet med ${(currentItem.daysLeft)*(-1)} dage."
+                    holder.background.setBackgroundColor(Color.parseColor("#CF3700"))
+                }
+                currentItem.daysLeft == -1 -> {
+                    holder.textView2.text = "Udløbet! Holdbarhed overskredet med ${(currentItem.daysLeft)*(-1)} dag."
+                    holder.background.setBackgroundColor(Color.parseColor("#CF6E00"))
+                }
+                currentItem.daysLeft == 0 -> {
+                    holder.textView2.text = "Udløber i dag."
+                    holder.background.setBackgroundColor(Color.parseColor("#DBD000"))
+                }
+                currentItem.daysLeft == 1 -> {
+                    holder.textView2.text = "Holdbar i ${currentItem.daysLeft} dag."
+                    holder.background.setBackgroundColor(Color.parseColor("#C5DB00"))
+                }
+                else -> {
+                    holder.textView2.text = "Holdbar i ${currentItem.daysLeft} dage."
+                    holder.background.setBackgroundColor(Color.parseColor("#58DB00"))
+            }
         }
     }
 
     override fun getItemCount() = exampleList.size
+
 
     inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
     View.OnClickListener{
         val imageView: ImageView = itemView.list_item_image_view
         val textView1: TextView = itemView.list_item_text_view_1
         val textView2: TextView = itemView.list_item_text_view_2
+        val background: RelativeLayout = itemView.item_layout
 
         init {
             itemView.setOnClickListener(this)
