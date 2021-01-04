05a0149f8b45e55772fa168b0465a90290a9a10f
Max <gammelgaard1993@hotmail.com>
2021-01-04 15:16:18 +0100

    Bottom navigation bar virker så man kan skifte mellem fragmenter.
    todo: flytt al kode fra aktiviter ind i fragmenter klasserne

--- app/src/main/java/com/app/bestb4/MainFragmentActivity.kt
+++ app/src/main/java/com/app/bestb4/MainFragmentActivity.kt
@@ -2,10 +2,41 @@ package com.app.bestb4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.app.bestb4.fragments.InfoFragment
+import com.app.bestb4.fragments.ListFragment
+import com.app.bestb4.fragments.SettingsFragment
+import kotlinx.android.synthetic.main.activity_main_fragment.*
 
 class MainFragmentActivity : AppCompatActivity() {
+
+
+    private val settingsFragment = SettingsFragment()
+    private val infoFragment = InfoFragment()
+    private val listFragment = ListFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_fragment)
+        replaceFragment(listFragment)
+
+
+        bottom_nav.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.ic_list -> replaceFragment(listFragment)
+                R.id.ic_settings -> replaceFragment(settingsFragment)
+                R.id.ic_info -> replaceFragment(infoFragment)
+
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
