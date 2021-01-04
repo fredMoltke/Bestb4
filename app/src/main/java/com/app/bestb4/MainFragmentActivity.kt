5805435029240d6f2878a364fcd8e1cf5249ceb5
Max <gammelgaard1993@hotmail.com>
2021-01-04 14:51:38 +0100

    påbegyndt at skift fra activities til fragmenter. fragmenter er opsat med xml. todo: flytt al kode fra aktiviter ind i fragmenter klasserne

---
+++ app/src/main/java/com/app/bestb4/MainFragmentActivity.kt
@@ -0,0 +1,11 @@
+package com.app.bestb4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainFragmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_fragment)
+    }
+}
