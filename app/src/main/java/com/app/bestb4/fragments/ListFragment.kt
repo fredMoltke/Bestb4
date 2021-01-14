8f7e0fc5a7d6ea9971872b4901ef1aa6cc0a7d38
fmoltke <freggi64@live.dk>
2021-01-14 20:23:50 +0100

    ryttet op i ListFragment klasse

--- app/src/main/java/com/app/bestb4/fragments/ListFragment.kt
+++ app/src/main/java/com/app/bestb4/fragments/ListFragment.kt
@@ -1,13 +1,8 @@
 package com.app.bestb4.fragments
 
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Matrix
-import android.net.Uri
 import android.os.Bundle
 import android.os.Parcelable
-import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -23,7 +18,6 @@ import kotlinx.android.synthetic.main.fragment_list.*
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
-import java.io.ByteArrayInputStream
 import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.collections.ArrayList
@@ -66,7 +60,6 @@ class ListFragment : Fragment() {
             startActivity(cameraIntent)
         }
 
-
     }
 
     fun insertItem(item: ListItem){
@@ -85,7 +78,6 @@ class ListFragment : Fragment() {
         Toast.makeText(activity, "Trykket på item ${clickEvent.position + 1}", Toast.LENGTH_SHORT).show()
     }
 
-    // IGNORE
     @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
     fun onItemEvent(itemEvent: ItemEvent){
         insertItem(itemEvent.item)
@@ -103,15 +95,9 @@ class ListFragment : Fragment() {
         EventBus.getDefault().unregister(this)
     }
 
-    override fun onPause() {
-        super.onPause()
-//        recyclerState = (recyclerView.layoutManager as LinearLayoutManager).onSaveInstanceState()!!
-    }
-
     override fun onResume() {
         super.onResume()
         itemList = insertionSort(itemList)
-//        recyclerView.layoutManager!!.onRestoreInstanceState(recyclerState)
     }
 
     //   https://chercher.tech/kotlin/insertion-sort-kotlin
@@ -144,74 +130,4 @@ class ListFragment : Fragment() {
         )
         return item.expiration - differenceInDays.toInt()
     }
-
-    fun byteArrayToBitmap(byteArray: ByteArray?): Bitmap {
-        val arrayInputStream = ByteArrayInputStream(byteArray)
-        return BitmapFactory.decodeStream(arrayInputStream)
-    }
-
-    private fun rotateImage(source: Bitmap, angle: Float): Bitmap {
-        val matrix = Matrix()
-        matrix.postRotate(angle)
-        return Bitmap.createBitmap(source, 0, 0, source.width, source.height, matrix, true)
-    }
-
-    private fun convertUriToBitmap(imageUri: Uri) : Bitmap{
-
-        val bitmap = MediaStore.Images.Media.getBitmap(activity?.contentResolver, imageUri)
-        return rotateImage(bitmap, 90F)
-    }
-
-//    fun generateBitmap() : Bitmap{
-//        val w: Int = 320
-//        val h: Int = 320
-//
-//        val conf = Bitmap.Config.ARGB_8888 // see other conf types
-//
-//        return Bitmap.createBitmap(w, h, conf)
-//    }
-
-// Funktion der opretter liste med dummy items
-//    private fun generateDummyList(size: Int): ArrayList<ListItem> {
-//        val list = ArrayList<ListItem>()
-//
-//        for (i in 0 until size) {
-//            val item = ListItem(R.drawable.ic_baseline_fastfood_24, "Item $i", "Line 2")
-//            list += item
-//        }
-//        return list
-//    }
-
-//    private fun generateSortingTest(): ArrayList<ListItem> {
-//        val simpleDateFormat = SimpleDateFormat("dd-MM-yyyy")
-//        val d1 = simpleDateFormat.parse("05-11-2020")
-//        val d2 = simpleDateFormat.parse("04-11-2020")
-//        val d3 = simpleDateFormat.parse("06-11-2020")
-//        val d4 = simpleDateFormat.parse("07-11-2020")
-//        val d5 = simpleDateFormat.parse("01-11-2020")
-//        val d6 = simpleDateFormat.parse("03-11-2020")
-//        val d7 = simpleDateFormat.parse("05-11-2020")
-//
-//        val testBitmap : Bitmap = generateBitmap()
-//
-//        val i1 = ListItem(1, "Et", 4, testBitmap, testBitmap, d1)
-//        val i2 = ListItem(2, "Fire", 9, testBitmap, testBitmap, d2)
-//        val i3 = ListItem(3, "To", 4, testBitmap, testBitmap, d3)
-//        val i4 = ListItem(4, "Tre", 4, testBitmap, testBitmap, d4)
-//        val i5 = ListItem(5, "Fem", 22, testBitmap, testBitmap, d5)
-//        val i6 = ListItem(6, "Syv", 23, testBitmap, testBitmap, d6)
-//        val i7 = ListItem(7, "Seks", 19, testBitmap, testBitmap, d7)
-//
-//        var arrayList = ArrayList<ListItem>()
-//        arrayList.add(i1)
-//        arrayList.add(i2)
-//        arrayList.add(i3)
-//        arrayList.add(i4)
-//        arrayList.add(i5)
-//        arrayList.add(i6)
-//        arrayList.add(i7)
-//
-//        return arrayList
-//    }
-
 }
