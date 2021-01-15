2b498e0f9350f988f20a71cf82c8f774241e53f1
fmoltke <freggi64@live.dk>
2021-01-15 21:23:05 +0100

    påbegyndt pænere design

--- app/src/main/java/com/app/bestb4/EditItemActivity.kt
+++ app/src/main/java/com/app/bestb4/EditItemActivity.kt
@@ -3,9 +3,10 @@ package com.app.bestb4
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class EditItemAcitivity : AppCompatActivity() {
+class EditItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_item_acitivity)
+        setContentView(R.layout.activity_edit_item_activity)
+
     }
 }
